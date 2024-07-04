454cc9062575e5b00ca2c25555108342e7c9d1a5
ankit7115 <ankitsrivastav5555@gmail.com>
2024-07-04 10:07:21 +0530

    requrements satisfied

--- Diabetes_pred/settings.py
+++ Diabetes_pred/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&wdl_!fx(xzn41c)4i_)7dkmdm8=c^e1dy$7fm0cbv#r8qy1g#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
